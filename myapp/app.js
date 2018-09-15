477568c95746a574b2fb21655dce45a8494a3b06
Saikat Guha <sktguha@gmail.com>
2018-09-16 00:55:22 +0530

    basic todo add working

--- myapp/app.js
+++ myapp/app.js
@@ -1,14 +1,26 @@
 define([
+    "dojo/_base/declare",
+    "dijit/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "dojo/text!./templates/maintemplate.html",
     "./todo"
-], function(todo) {
+], function(declare, _WidgetBase, _TemplatedMixin, template, todo) {
 
-    return {
-        init : function () {
-            var div = document.getElementById("mainDiv");
+    /**
+     * so make simple todo app
+     * widget will have a todo
+     */
+
+    return declare([_WidgetBase, _TemplatedMixin], {
+        templateString: template,
+        postCreate : function () {
             new todo({
-                text : "this is a text",
-                done : false
-            }).placeAt(div);
+                done : false,
+                text : "this is a text"
+            }).placeAt(this.todoList);
+        },
+        addTodo : function () {
+            alert("eee");
         }
-    }
+    });
 });
